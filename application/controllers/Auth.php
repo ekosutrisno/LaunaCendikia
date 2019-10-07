47d13119e252a5b9bed81ba67474428b8f0f9c2d
ekosutrisno <51039205+ekosutrisno@users.noreply.github.com>
2019-10-07 11:34:51 +0700

    improve

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -5,6 +5,9 @@ class Auth extends CI_Controller
 {
    public function index()
    {
+      $data['judul'] = 'Halaman Login';
+      $this->load->view('templates/auth_header', $data);
       $this->load->view('auth/login');
+      $this->load->view('templates/auth_footer');
    }
 }
